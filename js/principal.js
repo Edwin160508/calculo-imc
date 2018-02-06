5c0516c0fdf4f08cedefc3d93b19680afef830ac
Lima <edwin.pereira.o.lima@accenture.com>
2018-02-06 17:44:31 -0200

    Pintando background da linha.

--- js/principal.js
+++ js/principal.js
@@ -38,6 +38,6 @@ for(var i =0; i< pacientes.length; i++){
 	imc.textContent = calculoImc(peso, altura);
 	//pitando fonte em vermelho caso 'imc' for 0 siguinificando que 'Altura' ou 'Peso' estão inválidos.
 	if(imc.textContent === 'Peso inválido!' || imc.textContent === 'Altura inválida!'){
-		pacientes[i].style.color = 'red';
+		pacientes[i].style.backgroundColor = 'lightcoral';
 	}
 }
