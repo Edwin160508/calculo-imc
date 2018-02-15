f2fccb578cb459715cfc5befcdc15123baf8514f
Lima <edwin.pereira.o.lima@accenture.com>
2018-02-15 19:00:15 -0200

    Implementando Ajax Inicio.

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,30 @@
+var botaoBuscarPacientes = document.querySelector('#buscar-pacientes');
+
+botaoBuscarPacientes.addEventListener('click', function(event){
+	console.log('Buscando pacientes... ');
+	get('http://api-pacientes.herokuapp.com/pacientes');
+});
+
+function get(url){
+	return new Promise((resolve, reject)=>{
+		let xhr = new XMLHttpRequest();
+		xhr.open('GET', url);
+		xhr.addEventListener("load", function(event){
+			console.log(xhr.responseText);//
+			let pacientes = JSON.parse(xhr.responseText);
+			console.log(pacientes);
+		});
+		/*xhr.onreadystatechange = () =>{
+			if(xhr.readyState === 4){
+				if(xhr.status === 200){
+					resolve(JSON.parse(xhr.responseText));
+					console.log(xhr.responseText);
+				}else{
+					reject(xhr.responseText);
+					console.log(xhr.responseText);
+				}
+			}
+		};*/
+		xhr.send();
+	});
+}
