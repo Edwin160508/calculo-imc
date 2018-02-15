d8f80d76b3605173a81cd75b989f0a70559d8659
Lima <edwin.pereira.o.lima@accenture.com>
2018-02-15 16:11:38 -0200

    Otimizando o filtro, utilizando expressões regulares.

--- js/filtra.js
+++ js/filtra.js
@@ -2,13 +2,14 @@ var campoFiltro = document.querySelector('#filtrar-tabela');
 
 campoFiltro.addEventListener('input', function(event){
 	/* "this" equivale a campoFiltro */
-	var pacientes = document.querySelectorAll('.paciente');	
+	let pacientes = document.querySelectorAll('.paciente');	
 
 	if(this.value.length > 0){
 		/* com forEach "this" perdeu a referencia de campoFiltro passando a referenciar "Window" */
 		pacientes.forEach(function(paciente){ 
-			var nome = paciente.querySelector('.info-nome').textContent;		
-			if(nome != campoFiltro.value){
+			let nome = paciente.querySelector('.info-nome').textContent;
+			let expressaoRegular = new RegExp(campoFiltro.value, "i");		
+			if(!expressaoRegular.test(nome)){
 				paciente.classList.add('invisivel');
 			}else{
 				paciente.classList.remove('invisivel');
